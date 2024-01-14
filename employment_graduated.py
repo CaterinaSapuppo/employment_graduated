35bfb00ef18922c74fd2300773a8dfac9efecf17
CaterinaSapuppo <155912738+CaterinaSapuppo@users.noreply.github.com>
2024-01-14 23:17:30 +0100

    Update employment_graduated.py

--- employment_graduated.py
+++ employment_graduated.py
@@ -50,7 +50,7 @@ scatter_fig.update_layout(
 
 # Aggiungi etichette degli assi x e y
 scatter_fig.update_xaxes(title_text='Employment Rate 2021 (%)')
-scatter_fig.update_yaxes(title_text='Number of Graduates 2021', range=[-500, data['Number of Graduates 2021'].max() + 1000])
+scatter_fig.update_yaxes(title_text='Number of Graduates 2021', range=[0, data['Number of Graduates 2021'].max() + 1000])
 
 # Aggiungi linee degli assi x e y
 scatter_fig.update_xaxes(showline=True, linewidth=1, linecolor='black')
@@ -60,7 +60,7 @@ scatter_fig.update_layout(plot_bgcolor='white')
 # Imposta l'asse x con incrementi di 20%
 scatter_fig.update_xaxes(
     tickvals=list(range(0, 101, 20)),
-    ticktext=[f'{i}%' for i in range(0, 10, 20)],
+    ticktext=[f'{i}%' for i in range(0, 101, 20)],
     showticklabels=True,
 )
 
